e8eed09291777a8b0409ec59f014b5dd46b568b6
delskull <Deluta1989@gmail.com>
2024-04-21 14:50:22 +0300

    feat: complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((this.max - this.min) / 2 + this.min);
+        return this.number
     }
 
     lower() {
-
+        this.max = this.number;
+        this.guess();
     }
 
     greater() {
-
+      this.min = this.number;
+      this.guess();
     }
-}
+  }
 
 module.exports = GuessingGame;
